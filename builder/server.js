0242d31e25dd004fc241d6179f361d91acd772f6
Blake Elias <>
2023-12-09 21:12:30 +0000

    Serve index page via EJS

--- builder/server.js
+++ builder/server.js
@@ -1,8 +1,17 @@
 // Load Node modules
 var express = require('express');
+const ejs = require('ejs');
 // Initialise Express
 var app = express();
 // Render static files
 app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
 // Port website will run on
 app.listen(8080);
+
+// *** GET Routes - display pages ***
+// Root Route
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
