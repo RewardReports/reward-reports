6ae5378dfdd9d0a57e15707040645ba4ebe7abcb
Blake Elias <blakeelias@gmail.com>
2024-01-21 02:26:10 +0000

    Adding settings required for Heroku deployment (https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#3-add-a-procfile)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd builder; npm run start
--- builder/server.js
+++ builder/server.js
@@ -8,7 +8,9 @@ app.use(express.static('public'));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 // Port website will run on
-app.listen(8080);
+// Listen on port specified in 'PORT' environment variable:
+// https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
+app.listen(process.env.PORT);
 
 // *** GET Routes - display pages ***
 // Root Route
