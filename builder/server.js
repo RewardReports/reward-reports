c4df9fe750bdfe18bcf2b7af31d57b116c9d9ef0
Blake Elias <blakeelias@gmail.com>
2024-01-29 16:35:48 +0000

    Add mongodbRest configuration

--- builder/server.js
+++ builder/server.js
@@ -12,6 +12,61 @@ app.set('view engine', 'ejs');
 // https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
 app.listen(process.env.PORT);
 
+var mongodbRest = require('mongodb-rest/server.js');
+
+// MongoDB REST configuration:
+var mongodbRestConfiguration = {
+  "db": "mongodb://localhost:27017",
+  "endpoint_root": "server",
+  "server": {
+    "port": 3000,
+    "address": "0.0.0.0"
+  },
+  "accessControl": {
+    "allowOrigin": "*",
+    "allowMethods": "GET,POST,PUT,DELETE,HEAD,OPTIONS",
+    "allowCredentials": false
+  },
+  "dbAccessControl": {
+    "foo_database": ["collection1", "collection2"],
+    "bar_database": ["collection2", "collection3"],
+    "zoo_database": [],
+  },
+  "mongoOptions": {
+    "serverOptions": {
+    },
+    "dbOptions": {
+      "w": 1
+    }
+  },
+  "humanReadableOutput": true,
+  "urlPrefix": "",
+  "schema": {
+    "foo_database": {
+      "collection1": {
+        "definitions": {},
+        "$schema": "http://json-schema.org/draft-06/schema#",
+        "$id": "http://json-schema.org/draft-06/schema#",
+        "type": "object",
+        "properties": {
+          "value": {
+            "$id": "/properties/value",
+            "type": "boolean",
+            "title": "Foo boolean value",
+            "description": "An explanation about the purpose of this instance.",
+            "default": false,
+            "examples": [
+              false
+            ]
+          }
+        }
+      }
+    }
+  }
+}
+
+mongodbRest.startServer(mongodbRestConfiguration);
+
 // *** GET Routes - display pages ***
 // Root Route
 app.get('/', function(req, res) {
