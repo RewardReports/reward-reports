41433e778b0f16b4f2fece7d39a437ea030b0ddd
Blake Elias <blakeelias@gmail.com>
2024-02-20 03:25:25 +0000

    Attempt to fix 500 error by providing correct Date object type

--- builder/server.js
+++ builder/server.js
@@ -25,7 +25,7 @@ mongoose.connect(mongodbUri, {
 
 // Create Mongooose schema and model
 const ReportSchema = new mongoose.Schema({
-  date: String,
+  datetime: Date,
   markdownContent: String
 });
 
