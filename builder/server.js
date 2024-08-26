92c3b0ce8e069cd9c878d9c7dcb3d5491b70255b
Blake Elias <blakeelias@gmail.com>
2024-08-26 10:19:54 +0000

    Debugging

--- builder/server.js
+++ builder/server.js
@@ -171,7 +171,7 @@ app.get('/create-account', (req, res) => {
 
 // Create user
 app.post('/create-account', (req, res) => {
-  console.log("Create account request: ", req.body);
+  console.log("Create account request: ", req);
   User.create({
     username: req.body.username,
     organization_email: req.body.organization_email,
